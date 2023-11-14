d475f9d21b5e523a79e6a32b2d8350678e35b4b3
chisambwe.k <chisambwe.k@carbonyte.io>
2023-11-14 15:54:17 +0000

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>
+  return (
+    <Grid
+      templateAreas={{
+        base: `"user user" "chat profile" "main main"`,
+        lg: `"user profile" "chat main"`,
+      }}
+    >
+      <GridItem area="user" bg="green">
+        user
+      </GridItem>
+
+      <GridItem area="chat" bg="purple">
+        chat
+      </GridItem>
+
+      <GridItem area="profile" bg="coral">
+        profile
+      </GridItem>
+
+      <GridItem area="main" bg="grey">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
